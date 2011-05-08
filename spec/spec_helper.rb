2c5ac97972b4ec52a291b008f29c44d16e596c10
Jacob Helwig <jacob@technosorcery.net>
2011-05-08 12:26:15 -0700

    Add helper for reading file contents from spec/fixtures
    This will make reading fixture data from within spec/fixtures much
    easier in the specs, allowing us to move things like config files, and
    API response data into fixture files, and out of being directly in the
    spec files themselves.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,7 @@ require 'pull_request_bot'
 RSpec.configure do |config|
   config.mock_with :mocha
 end
+
+def read_fixture(file)
+  File.read(File.join(File.dirname(__FILE__), 'fixtures', file))
+end
